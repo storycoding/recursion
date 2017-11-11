01e8b630749e3d17ddc51446750f114ad9694892
Nuno <youknownuno@gmail.com>
2017-11-11 17:15:29 +0000

    stubbing parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,9 +2,78 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
 var parseJSON = function(json) {
-  // your code goes here
+
+	//CHAR-BY-CHAR PROGRESSION
+
+
+  	//evaluate char values and progress down the chain as in http://json.org/
+ 	var next = function() {
+
+	  	//breaks if string length === 0
+
+	  	//identifies the first character in the string;
+
+	  	//removes that first character;
+
+	  		//calls the next step according to current char
+
+	  	
+	  	//loop
+
+  	}
+
+  	//ignore all spaces
+  	var cutSpace = function(string) {
+  		//if first character in string is a space
+  			//cut the first space
+  			//loop
+  		//else - proceed
+
+  	}
+  
+  	//iterate through all the breakpoints and decides what to call next
+  	var condition = function(string) {
+  		//if char is (type)
+  			//treat body as a var of that type
+
+
+  	}
+
+
+
+  //start with value types	
+  	// { is followed by object
+
+  	// [ is followed by array
+
+  	// " is followed by string
+  	// - or 0-9 is followed by number
+
+  	//go into number
+
+
+  	//parsing example:
+  	var stringToObj = function(string) {
+		var object = {};
+		var key // = part of string
+		var value // = part of string
+		object[key] = value;
+
+		return object;
+		}
+
+	//somewhere in stringToObj it needs to check for the inside values (if chain)
+
 };
+
+
+
+//flow:
+
+// next - condition - getElementType ...
+
 
 
 
