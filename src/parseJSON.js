b1924a03f5798b9972fafe830c33c12a5f283d07
storycoding <30328808+storycoding@users.noreply.github.com>
2017-11-12 17:41:56 +0000

    switching computers

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,127 +1,306 @@
-// this is what you would do if you were one to do things the easy way:
-// var parseJSON = JSON.parse;
+//===========================================//
+//============ helper functions =============//
+//===========================================//
 
-// but you're not, so you'll write it from scratch:
+var cutAllSpaces = function(string) {
+	var spacedOutString = "";
+	var insideQuotes = false;
+
+
+	//cuts out the spaces outside quote marks ""
+	//stores the result in a new string
+	
+	
+	while (string.length >= 1) {
+
+		while (!insideQuotes) {
+			if (string.charAt(0) === " ") {
+				string = string.substr(1);
+
+		//check if we're etering quotes
+			} else if (string.charAt(0) === "\"") {
+				string = string.substr(1);
+				insideQuotes = !insideQuotes; //swaps to true
+
+			} else {
+				spacedOutString += string.charAt(0);
+				string = string.substr(1);
+			}
+			
+		  if	(string.length < 1) {
+		    break;
+		  }
+			}
+
+
+		//inside quotes, add everything
+ 		while (insideQuotes) {
+			if (string.charAt(0) === "\"") {
+			  string = string.substr(1);
+				insideQuotes = !insideQuotes; //swaps to false
+			
+			} else {
+				spacedOutString += string.charAt(0);
+				string = string.substr(1);
+ 			}
+		}
+		
+  if	(string.length < 1) {
+		    break;
+		  }
+
+	}
+	
+
+	return spacedOutString;
+};
+
+
+//===========================================//
+//============ array or object ==============//
+//===========================================//
+
+	var takeObject = function(string) {
+  		var object = {};
+  		pair(string,object);
+  	};
+
+
+  	var takeArray = function(string) {
+  		var array = [];
+  		sequence(string,array);
+  	};
+
+
+//===========================================//
+//============ JSON progression =============//
+//===========================================//
+
+	//evaluate char values and progress down
+	//the chain as in http://json.org/
+
+	var next = function(string) {
+
+		//breaks if string length === 0
+		if (string.length === 0) {
+			return;
+		}
+
+  	condition(string);
+  	//identifies the first character in the string;
+
+  	//removes that first character;
+
+  		//calls the next step according to current char
+
+  	
+  	//loop
+
+	};
+
+
+
+	//iterate through all the breakpoints and decides what to call next
+	var condition = function(string) {
+
+		if (string.charAt(0) === "{" ) {
+			//run obj
+
+		} else if (string.charAt(0) === "[") {
+			//run arr
+
+		} else if (string.charAt(0) === "\"") { // consider escape
+			takeString(string);
+		} else if (string.charAt(0) === "\\") {
+			takeString(string);
+		} else if (string.charAt(0) === "\/") {
+			takeString(string);
+		} else if (string.charAt(0) === "\b") {
+			takeString(string);
+		} else if (string.charAt(0) === "\f") {
+			takeString(string);
+		} else if (string.charAt(0) === "\n") {
+			takeString(string);
+		} else if (string.charAt(0) === "\r") {
+			takeString(string);
+		} else if (string.charAt(0) === "\t") {
+			takeString(string);
+		//} else if (string.charAt(0) === "\u") { - \u causes error
+			//run string
+
+		} else if (string.charAt(0) === "-") {
+			takeNumber(string);
+
+		} else {
+			for (var i = 0; i <= 9; i++) {
+				if (string.charAt(0) === i.toString()) { //CHECK THIS
+					takeNumber(string);
+				}
+			}
+		}
+
+	return;
+	};
+
+
+
+//start with value types	
+	// { is followed by object
+
+	// [ is followed by array
+
+	// " is followed by string
+	// - or 0-9 is followed by number
+
+	//go into number
+
+
+	//parsing example:
+
+
+//===========================================//
+//============ grabbing elements ============//
+//===========================================//
+
+var takeString = function(string) { //BEWARE ESCAPEES
+	var text = "";
+	//break if it reaches "\""
+	while(true) {
+		text += string.charAt(0);
+		string = string.substr(1);
+		
+		if (string.charAt(0) === "\"") {
+			break;
+		}
+	}
+
+	return text;
+};
+
+
+
+var takeNumber = function(string) {
+	var text = "";
+	//break if it reaches "," or a space or "}" or "]"
+	while (true) {
+		text += string.charAt(0);
+		string = string.substr(1);
+		
+		if (string.charAt(0) === "," ||
+		string.charAt(0) === "}" ||
+		string.charAt(0) === "]" ||
+		string.charAt(0) === " " ) {
+			break;
+		}
+	}
+
+	number = parseFloat(text);
+	return number;
+};
+
+
+
+var takeNullTrueFalse = function(string) {
+	var type = "";
+
+	while (true) {
+		type += string.charAt(0);
+		string = string.substr(1);
+		
+		if (string.charAt(0) === "," ||
+		string.charAt(0) === "}" ||
+		string.charAt(0) === "]" ||
+		string.charAt(0) === " " ) {
+			string = string.substr(1);
+			break;
+		}
+	}
+
+
+	if (type === "true") {
+		return true;
+
+	} else if (type === "false") {
+		return false;
+
+	} else if (type === "null") {
+		return null;
+
+	} else {
+		console.log("failed to define type.");
+		console.log("string state: " + string);
+		console.log("type state: " + type);
+		return;
+	}
+
+};
+
+
+
+//===========================================//
+//=========== adding and pushing ============//
+//===========================================//
+
+	
+	//takes an obj and gives it a pair from string
+	var pair = function(string,object) { // CHECK FOR SPACES
+		var key = (takeString(string));
+		var value = "";
+
+
+		object[key] = condition(string); // CHECK FOR NESTED OBJS
+		if (string.charAt(0) === ",") {
+			string = string.substr(1);
+			pair(string,object);
+		}
+	};
+
+	//will keep adding until it reaches a "]"
+	var sequence = function(string,array) {
+		var value = condition(string);
+		array.push(value);
+		if(string.charAt(0) !== "]") {
+			sequence(string,array);
+		}
+	};
+
+
+
+
+
+//===========================================//
+//================ parseJSON ================//
+//===========================================//
 
 var parseJSON = function(json) {
+
+	//rid of pointless spaces
+	json = cutAllSpaces(json);
+
+	//the parent
 	var parsedObject;
-	//CHAR-BY-CHAR PROGRESSION
 
+	//object or array?
+	if (string.charAt(0) === "{") {
+		string = string.substr(1);
+		parsedObject = takeObject(json);
 
-  	//evaluate char values and progress down the chain as in http://json.org/
- 	var next = function(string) {
- 		if (string.length === 0) {
- 			return;
- 		}
+	} else if (string.charAt(0) === "[") {
+		string = string.substr(1);
+		parsedObject = takeArray(json);
+	}
+	// somewhere in stringToObj it needs to
+	// check for the inside values (if chain)
 
- 		cutSpace(string);
-	  	//breaks if string length === 0
-
-	  	//identifies the first character in the string;
-
-	  	//removes that first character;
-
-	  		//calls the next step according to current char
-
-	  	
-	  	//loop
-
-  	}
-
-  	//ignore all spaces
-  	var cutSpace = function(string) {
-  		//if first character in string is a space
-  			//cut the first space
-  			//loop
-  		
-  		if (string.charAt(0) === 0) {
-  			string = string.trim();
-  			cutSpace(string);
-  		}
-  		//else - proceed
-  		return string;
-  		
-  	};
-  
-  	//iterate through all the breakpoints and decides what to call next
-  	var condition = function(string) {
-
-  		if (string.charAt(0) === "{" ) {
-  			//run obj
-
-  		} else if (string.charAt(0) === "[") {
-  			//run arr
-
-  		} else if (string.charAt(0) === "\"") { // consider escape
-  			//run string
-  		} else if (string.charAt(0) === "\\") {
-  			//run string
-  		} else if (string.charAt(0) === "\/") {
-  			//run string
-  		} else if (string.charAt(0) === "\b") {
-  			//run string
-  		} else if (string.charAt(0) === "\f") {
-  			//run string
-  		} else if (string.charAt(0) === "\n") {
-  			//run string
-  		} else if (string.charAt(0) === "\r") {
-  			//run string
-  		} else if (string.charAt(0) === "\t") {
-  			//run string
-  		//} else if (string.charAt(0) === "\u") { - \u causes error
-  			//run string
-
-  		} else if (string.charAt(0) === "-") {
-  			//run number
-  		} else {
-  			for (var i = 0; i <= 9; i++) {
-  				if (string.charAt(0) === i.toString()) {
-  					//run number
-  				}
-  			}
-  		}
-
-  	return;
-  	};
-
-
-
-  //start with value types	
-  	// { is followed by object
-
-  	// [ is followed by array
-
-  	// " is followed by string
-  	// - or 0-9 is followed by number
-
-  	//go into number
-
-
-  	//parsing example:
-  	var stringToObj = function(string) {
-  		//break if it reaches a ""
-		var object = {};
-		var key // = part of string
-		var value // = part of string
-		object[key] = value;
-
-		return object;
-		};
-
-	//somewhere in stringToObj it needs to check for the inside values (if chain)
 	return parsedObject;
 };
 
 
 
-//flow:
+//===========================================//
+//============ compare results ==============//
+//===========================================//
 
-// next - condition - getElementType ...
-
-
-
-
-//testing my function vs the original
 var compareParse = function() {
 
 	parseableStrings.forEach(function(test) {
@@ -136,4 +315,4 @@ var compareParse = function() {
     console.log("=======================================");
     });
 
-}
+};
